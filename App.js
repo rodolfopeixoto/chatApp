7da0fbc82d94a8c320153eed7a768b5242a62262
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-05-03 11:20:20 -0300

    ChatApp complete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import { send, subscribe } from 'react-native-training-chat-server';
 
 const NAME   = 'Rodolfo Peixoto';
@@ -27,6 +27,18 @@ export default class App extends React.Component {
     );
   }
 
+  async sendMessage(){
+    await send({
+      channel: CHANEL,
+      sender: NAME,
+      message: this.state.typing
+    });
+
+    this.setState({
+      typing: '',
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -35,15 +47,20 @@ export default class App extends React.Component {
           renderItem={this.renderItem}
           inverted
         />
+        <KeyboardAvoidingView behavior="padding">
         <View style={styles.footer}>
           <TextInput 
             value={this.state.typing}
             onChangeText={ (text) => { this.setState({typing: text}) }}
             style={styles.input}
             underlineColorAndroid="transparent"
-            placeholder={Type something nice}
+            placeholder="Type something nice"
           />
         </View>
+        </KeyboardAvoidingView>
+        <TouchableOpacity onPress={ this.sendMessage.bind(this) }>
+          <Text style={styles.send}>Send</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -75,5 +92,12 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     fontSize: 18,
     flex: 1
+  },
+  send: {
+    alignSelf: 'center',
+    color: 'lightseagreen',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 20
   }
 });
